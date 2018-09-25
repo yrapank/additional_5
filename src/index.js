18f9ad894b99c5d6c313549b3848c7970539dc86
Yury Panamarou <yra.pank@gmail.com>
2018-09-25 15:48:10 +0300

    I have tried

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bracketsConfig) { 
+  let arr1 =[]; 
+  let arr2 = []; 
+  var answer;  
+  for(let i = 0, arrLength =bracketsConfig.length;i<arrLength;i++ ){ 
+    arr1.push(bracketsConfig[i][0]);  
+    arr2.push(bracketsConfig[i][1]); 
+  } 
+  for(let j =0;j<str.length;j++){  
+    for(let i = 0; i<arr1.length;i++){ 
+      if(str[j]===arr1[i]){ 
+        break; 
+      } 
+      else if(j == 0){ 
+        return answer = false; 
+      } 
+      else if(str[j]===arr2[i]){ 
+        break; 
+      } 
+      return answer = false; 
+    } 
+  return answer = true; 
+  } 
+  return answer; 
+  };
+  
